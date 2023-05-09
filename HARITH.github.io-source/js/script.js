3c64aaf8e7feff488de0b6c463b646716dc3a95e
aimanhrith99 <119168259+aimanhrith99@users.noreply.github.com>
2023-05-09 12:09:24 +0800

    Improves

---
+++ HARITH.github.io-source/js/script.js
@@ -0,0 +1,41 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function () {
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navlist.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("bx-x");
+  navlist.classList.remove("active");
+};
+
+const sr = ScrollReveal({
+  distance: "0px",
+  duration: 1700,
+  reset: true,
+});
+
+sr.reveal(".home-text", { delay: 350, origin: "left" });
+sr.reveal(".home-img", { delay: 350, origin: "right" });
+sr.reveal(".party", { delay: 350, origin: "top" });
+
+sr.reveal(".sub-service,.about,.portfolio,.service,.cta,.contact", {
+  delay: 100,
+  origin: "top",
+});
+
+var typingEffect = new Typed(".multiText", {
+  strings: ["Nuraiman Harith", "Coder", "Web Developer"],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 1500,
+});
